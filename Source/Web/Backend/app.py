9a1ee476f93a8cafbbac176e5530ef7da2291417
Colton Boyd <coltonboyd503@icloud.com>
2024-08-25 13:06:35 -0400

    Now serving index.html from flask app. Also included some card width changes. Closes #9.

--- Source/Web/Backend/app.py
+++ Source/Web/Backend/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_folder='../Frontend',
+            static_url_path='/')
 
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+def index():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
